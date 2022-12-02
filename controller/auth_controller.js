ca237393fafc972cf475fa17c4bebcb98c4b3d81
azuda <aaronzhang587@gmail.com>
2022-12-02 13:45:21 -0800

    Test code login/register

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,4 +1,5 @@
 let database = require("../database");
+const userModel = require("../models/userModel").userModel;
 
 let authController = {
   login: (req, res) => {
@@ -10,11 +11,36 @@ let authController = {
   },
 
   loginSubmit: (req, res) => {
-    // implement
+    // implement login logic
+
+    // const { email, password } = req.body;
+    // const user = userModel.findOne(email);
+    // if (user.password === password) {
+    //   req.session.user = user;
+    //   res.redirect("/reminders");
+    // } else {
+    //   res.redirect("/login");
+    // }
   },
 
   registerSubmit: (req, res) => {
-    // implement
+    // implement register logic
+
+    // const { name, email, password } = req.body;
+    // const user = userModel.findOne(email);
+    // if (user) {
+    //   res.redirect("/register");
+    // } else {
+    //   const newUser = {
+    //     id: database.length + 1,
+    //     name,
+    //     email,
+    //     password,
+    //   };
+    //   database.push(newUser);
+    //   req.session.user = newUser;
+    //   res.redirect("/reminders");
+    // }
   },
 };
 
